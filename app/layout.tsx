78a597d1f07228df414beb14596b9f98681bb935
satish1872 <41827034+satish1872@users.noreply.github.com>
2024-04-19 09:24:55 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 
+/*
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
+*/
+
+const defaultUrl= `https://spatial-data-analyser-8nskdptv9.vercel.app`;
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
